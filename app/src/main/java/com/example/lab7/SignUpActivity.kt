dbe9ede6a22dcc5b8bf4497cca7d36627514e9b7
Fildrix <vip2maks@gmail.com>
2025-04-02 15:30:58 +0500

    added styles and changed SignUpActivity

--- app/src/main/java/com/example/lab7/SignUpActivity.kt
+++ app/src/main/java/com/example/lab7/SignUpActivity.kt
@@ -1,20 +1,49 @@
 package com.example.lab7
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var edUsername: EditText
+    private lateinit var edPassword: EditText
+    private lateinit var edConfirmPassword: EditText
+    private lateinit var btnCreateUser: Button
+
+    private val CREDENTIAL_SHARED_PREF = "our_shared_pref"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Инициализация
+        edUsername         = findViewById(R.id.ed_username)
+        edPassword         = findViewById(R.id.ed_password)
+        edConfirmPassword  = findViewById(R.id.ed_confirm_pwd)
+        btnCreateUser      = findViewById(R.id.btn_create_user)
+
+        btnCreateUser.setOnClickListener {
+            val strPassword = edPassword.text.toString()
+            val strConfirm = edConfirmPassword.text.toString()
+            val strUsername = edUsername.text.toString()
+
+            // Проверяем совпадение паролей
+            if (strPassword.equals(strConfirm, ignoreCase = true)) {
+                val preferences: SharedPreferences = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE)
+                val editor = preferences.edit()
+                editor.putString("Password", strPassword)
+                editor.putString("Username", strUsername)
+                editor.apply()
+
+                Toast.makeText(this, "User created!", Toast.LENGTH_SHORT).show()
+                finish() // Закрываем экран регистрации
+            } else {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
